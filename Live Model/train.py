1fb8a75d3ac18415910cf5cb79e8231693e61744
Xaviemille <xaviemiello@gmail.com>
2026-05-26 11:47:49 +0700

    Second Commit

--- Live Model/train.py
+++ Live Model/train.py
@@ -24,4 +24,13 @@ def train_one_epoch(model, loader, optimiser, scaler, device):
         total_loss += loss.item() * imgs.size(0)
     return total_loss / len(loader.dataset)
 
-
+@torch.no_grad()
+def evaluate(model, loader, device):
+    model.eval()
+    total_loss = 0.0
+    for imgs, labels in tqdm(loader, desc='  valid', leave=False):
+        imgs   = imgs.to(device)
+        labels = labels.to(device)
+        preds  = model(imgs)
+        total_loss += mse_loss(preds, labels).item() * imgs.size(0)
+    return total_loss /
